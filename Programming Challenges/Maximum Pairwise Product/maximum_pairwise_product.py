d4d44b15d6bc3a98c52d7321f2376a6b455ea216
pujith22 <pujith22.sde@gmail.com>
2025-05-05 23:35:18 +0530

    completed max_pairwise_product assignment.

--- Programming Challenges/Maximum Pairwise Product/maximum_pairwise_product.py
+++ Programming Challenges/Maximum Pairwise Product/maximum_pairwise_product.py
@@ -17,7 +17,17 @@ def max_pairwise_product_naive(numbers):
 def max_pairwise_product(numbers):
     assert len(numbers) >= 2
     assert all(0 <= x <= 2 * 10 ** 5 for x in numbers)
-    type here
+    maxi = -float("Inf")
+    maxPos = -1
+    for i in range(len(numbers)):
+        if(numbers[i]>maxi):
+            maxi = numbers[i]
+            maxPos = i
+    secondMaxi = -float("Inf")
+    for i in range(len(numbers)):
+        if i!=maxPos and numbers[i]>secondMaxi:
+            secondMaxi = numbers[i]
+    return maxi * secondMaxi
 
 
 if __name__ == '__main__':
